cf66408766d6bbb56f5fea7b5211842e2e8fdb5b
Shabrina Maharani <130067945+Maharanish@users.noreply.github.com>
2023-11-26 23:10:51 +0700

    all

--- src/PDA.py
+++ src/PDA.py
@@ -1,5 +1,6 @@
 import os
 from stack import Stack
+from colorama import Fore, Style
 
 class PDA:
     def __init__(self, state, alphabet, transition, startState, startStackSymbol, symbol, accepting_states) -> None:
@@ -17,47 +18,46 @@ class PDA:
         statebaru = self.startState
         topstackbaru = self.startStackSymbol
         self.stack.push(topstackbaru)
-        print("Awal Transition: Stack =", self.stack.get_items(), "Top =", self.stack.top())
-
+        #print("Awal Transition: Stack =", self.stack.get_items(), "Top =", self.stack.top())
+        line = 1
         for symbol in input_list:
             if symbol == '\n':
-                symbol = '!'
+                symbol = '|'
+                line+=1
             if symbol == ' ':
-                symbol = '!'
+                symbol = '|'
             print("Current State:", statebaru)
             print("Current Symbol:", symbol)
             if statebaru == '':
                 return False
             
             found_transition = False
-            print("Before Transition: Stack =", self.stack.get_items(), "Top =", topstackbaru)
-
             if statebaru in self.transition:
-                print("Transition State:", statebaru)
-                print("Transition Rules:", self.transition[statebaru])
+                #print("Transition State:", statebaru)
+                #print("Transition Rules:", self.transition[statebaru])
                 transition_dict = self.transition[statebaru]
                 if symbol in transition_dict:
                     if self.stack.top() in transition_dict[symbol]:
                         next_state, push_stack = transition_dict[symbol][self.stack.top()]
                     else:
+                        print(Fore.RED + "UPS! Something wrong! Invalid transition in line", line, "for symbol", symbol, "and top of stack", self.stack.top() + Style.RESET_ALL)
                         return False
                 else:
+                    print(Fore.RED + "UPS! Something wrong! Invalid symbol", symbol, "for state", statebaru, "in line", line, Style.RESET_ALL)
                     return False
 
                 found_transition = True
-                top_stack = self.stack.top()
-                print(top_stack)
                 if push_stack == '$':
                     self.stack.pop()  # Pop the top of the stack if the stack operation is not empty
-                    print("Before Transition: Stack =", self.stack.get_items(), "Top =", topstackbaru)
+                    #print("After pop: Stack =", self.stack.get_items(), "Top =", topstackbaru)
                     top_stack = self.stack.top()
-                    print(top_stack)
+                    #print(top_stack)
                 else :
                     self.stack.pop()
-                    print("Before Transition: Stack =", self.stack.get_items(), "Top =", topstackbaru)
+                    #print("after pop: Stack =", self.stack.get_items(), "Top =", topstackbaru)
                     self.stack.push(push_stack)  # Push symbols onto the stack in reverse order
                     top_stack = self.stack.top()
-                    print(top_stack)
+                    #print(top_stack)
                 
                 statebaru = next_state
                 
--- src/main.py
+++ src/main.py
@@ -1,54 +1,77 @@
 from PDA import PDA
 from myparser import parse_pda_file
+import time
+import sys
+from colorama import Fore, Style
 
-# Example usage:
-print("mulai")
-#state_set = {'p', 'q', 'r'}
-#alphabet_set = {'0', '1'}
-#transition_rules = {
-#    'p': {
-#        '0': {'#':('p','#0'),
-#              '0':('p','00'),
-#            },
-#        '1': {'0':('q', ''),},
-#        '!': {'#': ('r', ''),},
-#    },
-#    'q': {
-#       '1': {'0':( 'q', ''),},
-#        '!': {'#': ('r', ''),},
-#    },
-#}
+def print_colored_ascii_art():
+    art = """Hiii! Welcome to HTML Parser by Semoga Hoki AAMIIN!:D⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣠⣤⣤⣄⣀⡀⠀⠀⠀⠀⠀⣴⡟⠉⠀⠀⠀⠀⠙⢻⣆⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⠾⠛⠋⠉⠉⠉⠉⠙⠻⢷⣤⡀⠀⠀⣼⠏⠀⠀⠀⠀⠀⠀⠀⣿⡆
+    ⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⡄⣼⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡇
+    ⠀⠀⠀⠀⠀⠀⠀⢀⣿⠃⠀⠀⠀⠀⠀⠀⠀⣀⣰⣶⡆⠀⠀⠘⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⠁
+    ⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⢰⣿⡖⠒⠈⠉⠀⠀⠉⠀⠀⠀⢠⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⠇⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠘⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣞⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⠏⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⢹⣷⣄⡀⠀⠀⠀⠀⠀⠀⣀⣠⠴⠚⠁⠈⢧⡀⠀⠀⠀⠀⠀⢀⣴⡿⠋⠀⠀⠀
+    ⠀⠀⠀⠀⠀⢀⣴⠟⢫⠿⠉⠛⠒⠒⠒⠛⠉⠉⠀⢀⠤⢄⠀⡐⢷⠀⠀⠀⣠⣴⠿⠋⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⢠⣿⢫⠀⠀⢰⣃⣀⣇⣀⣀⣀⣀⣀⣀⣀⣉⣀⣀⣀⣀⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⢠⡿⠁⡈⠀⣠⠏⠀⠰⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠡⠀⠙⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⢠⣿⠃⡐⠁⣰⠃⠀⡰⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠡⡀⠘⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⣼⡟⠊⠀⢠⣏⠤⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠢⣸⣷⠀⠀⠀⠀⠀⠀⠀⠀
+    ⢠⣿⠀⠀⠀⣼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⡇⠀⠀⠀⠀⠀⠀⠀
+    ⢸⡟⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀
+    ⢸⣇⠀⠀⠀⠸⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⠃⠀⠀⠀⠀⠀⠀⠀
+    ⠘⣿⠀⠀⠀⠀⠹⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⠏⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⢻⣧⠀⠀⠀⠀⢸⠳⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⢻⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠻⣧⣀⠀⠀⣸⡆⠀⡝⠓⠦⣤⣀⣀⡀⠀⠀⠀⣀⣀⣤⠴⠚⢹⠀⣸⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠈⠛⠛⠛⢻⣧⠜⠀⠀⠀⠀⠀⠈⠉⢹⣏⠉⠉⠀⠀⠀⠀⠀⢣⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⢻⣦⠀⠀⠀⠀⠀⠀⠀⣾⣿⡀⠀⠀⠀⠀⠀⢠⣾⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣷⣄⡀⠀⠀⠀⣴⡟⢻⣷⣀⠀⣀⣠⣴⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠿⠿⠟⠋⠀⠀⠙⠛⠿⠟⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    """
+
+    colored_art = ""
+    colors = [Fore.LIGHTYELLOW_EX, Fore.RED, Fore.YELLOW, Fore.BLUE, Fore.YELLOW, Fore.CYAN, Fore.GREEN]
+    color_index = 0
+
+    for char in art:
+        if char == '\n':
+            colored_art += char
+        elif char == ' ':
+            colored_art += char
+        else:
+            colored_art += colors[color_index % len(colors)] + char + Style.RESET_ALL
+            color_index += 1
+
+    print(colored_art)
+
+# Call the function to print the colored ASCII art
+print_colored_ascii_art()
 
 
-
-#start_state = 'p'
-#symbol_stack = {'#', '0', '1'}
-#start_stack_symbol = '#'
-#accepting_states = {'r'}
-
+print("Please input your PDA rules's path directory below!")
 file_path = input()
+print()
+print("Thank you!")
+print()
 with open(file_path, 'r', encoding='utf-8') as file:
-    # your code here
     state, alphabet, transition, start_state, start_stack_symbol, stack_symbol, accepting_states = parse_pda_file(file_path)
-
-    # Create an instance of PDA
 pda = PDA(state, alphabet, transition, start_state, start_stack_symbol, stack_symbol, accepting_states)
-
-    # Example: Print the parsed information
-print("State:", state)
-print("Alphabet:", alphabet)
-print("Transition:", transition)
+print("Let's see your PDA's data!")
+#print("State:", state)
+#print("Alphabet:", alphabet)
+#print("Transition:", transition)
 print("Start State:", start_state)
 print("Start Stack Symbol:", start_stack_symbol)
-print("Stack Symbol:", stack_symbol)
+#print("Stack Symbol:", stack_symbol)
 print("Accepting States:", accepting_states)
 
-print("cek2")
-
-# Process input
-# Specify the path to your HTML file
+print()
+print("Please input your HTML file's path directory below!")
 html_file_path = input("")
-
+print()
+print("Thank you!")
+print()
 try:
     with open(html_file_path, 'r', encoding='utf-8') as file:
         html_content = file.read()
@@ -59,5 +82,12 @@ except UnicodeDecodeError as e:
     print(f"Error decoding file: {e}")
     exit(1)
 
+
 result = pda.process(html_content)
-print("result:", result)
+
+print("──────────────────────── ⋆⋅☆⋅⋆ ────────────────────────")
+if result == True:
+    print(f"{Fore.YELLOW}  OKAY! GOOD JOB! YOUR HTML FILE HAS CORRECT SYNTAX :) {Style.RESET_ALL}")
+else:
+    print(f"{Fore.YELLOW}<----------YOUR HTML FILE HAS SYNTAX ERROR :(----------->{Style.RESET_ALL}")
+print("──────────────────────── ⋆⋅☆⋅⋆ ────────────────────────")
